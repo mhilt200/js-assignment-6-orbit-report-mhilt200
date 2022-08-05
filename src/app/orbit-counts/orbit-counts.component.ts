2223de6b5074c0edb839abd834722e230ea5dd62
Michael Hilt <michael_hilt@comcast.com>
2022-08-05 08:48:08 -0400

    made it look like the gif minus bonus missions

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { empty } from 'rxjs';
 import { Satellite } from '../satellite';
 
 @Component({
@@ -15,7 +16,7 @@ export class OrbitCountsComponent implements OnInit {
   ngOnInit() {
   }
 
-  countByType(type: string): number {
+	countByType(type: string): number {
 	let count = 0;
 	if (this.satellites) {
 	  for (let i = 0; i < this.satellites.length; i++) {
@@ -27,7 +28,17 @@ export class OrbitCountsComponent implements OnInit {
 	return count;
  }
 
-satelliteTypes = ['Communication', 'Positioning', 'Probe', 'Space Debris', 'Space Station', 'Telescope'];
+ typeArr: string[] = ["Communication", "Positioning", "Probe", "Space Debris", "Space Station", "Telescope"];
 
+// 	typeNames(): string[] {
+// 		let emptyArr: string[] = [];
+
+// 		for (let i = 0; i < this.satellites.length; i++) {
+// 			if (!emptyArr.includes(this.satellites['type'])) {
+// 			emptyArr.push(this.satellites['type']);
+// 		}
+// 	}
+// 		return emptyArr;
+//  }
 
 }
